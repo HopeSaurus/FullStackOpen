d4685abdc28208ce2f2cade2a246bc530aa3b0a7
HopeSaurus <hopeXime@outlook.com>
2025-06-09 13:56:42 -0600

    Add exercise 1.8, 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,50 @@
 import { useState } from 'react'
 
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Statistics = (props) => {
+  const good = props.good
+  const bad = props.bad
+  const neutral = props.neutral
+  const all = props.all
+    
+  if(all <= 0){
+    return (
+    <>
+    <Header text="statistics" />
+    <p>No feedback given</p>
+    </>
+    )
+  }
+  return (
+  <>
+  <Header text="statistics" />
+  <p>good {good}</p>
+  <p>neutral {neutral}</p>
+  <p>bad {bad}</p>
+  <p>all {all}</p>
+  <p>average {(all > 0)?(good - bad) / all: 0}</p>
+  <p>positive {(all > 0)?good / all * 100: 0}%</p>
+  </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
-
-  const Header = ({text}) => {
-    return (
-      <h1>{text}</h1>
-    )
-  }
-  const Button = ({onClick, text}) => {
-    return (
-      <button onClick={onClick}>{text}</button>
-    )
-  }
 
   return (
     <div>
@@ -34,14 +62,7 @@ const App = () => {
         setAll(all + 1)
       }} text="bad" />
 
-      <Header text="statistics" />
-
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(all > 0)?(good - bad) / all: 0}</p>
-      <p>positive {(all > 0)?good / all * 100: 0}%</p>
+      <Statistics good = {good} bad = {bad} neutral = {neutral} all = {all}/>
     </div>
   )
 }
