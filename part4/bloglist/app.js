e3f058922aba1f4cd9a4b17714f1cc24026230a2
HopeSaurus <hopeXime@outlook.com>
2025-09-19 17:45:17 -0600

    Exercise 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,27 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const app = express()
 
-const Blog = require('./models/blog')
-
-mongoose.connect(config.MONGODB_URL)
-
+logger.info('connecting to ', config.MONGODB_URL)
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(()=>{
+    logger.info('Connection successful')
+  })
+  .catch((error)=>{
+    logger.error('error connecting to MongoDB', error.message)
+  })
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+app.use('/api/blogs',blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
