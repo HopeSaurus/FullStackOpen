c03c57b27f6fb84332d87d4e59cfa38f256f35c9
HopeSaurus <hopeXime@outlook.com>
2025-09-19 15:43:27 -0600

    Exercise 4.1

---
+++ part4/bloglist/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://hopefulxime:z9dJbSxyTdGPJARU@fullstackopen.qieypkk.mongodb.net/?retryWrites=true&w=majority&appName=fullstackopen'
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+
+module.exports = app
